77e7ee3f71072faf344b1f3ced46830373bc57c0
JSDID <icegouse@gmail.com>
2025-12-12 06:35:46 +0200

    Add top page title

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -42,6 +42,11 @@ const team = [
 export default function About() {
   return (
     <main className={styles.about}>
+      
+      {/* Заголовок сторінки */}
+      <header className={styles.about__header}>
+        <h1 className={styles.about__pageTitle}>Про нас</h1>
+      </header>
 
       {/* Наша місія */}
       <Section title="Наша місія">
